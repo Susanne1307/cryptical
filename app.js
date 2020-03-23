c596d6835cbff303ae22999c7f3b3a76da230da5
Susanne1307 <susanneholocher@mail.de>
2020-03-23 15:43:45 +0100

    Add get and set password functions

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-function sayHi() {
-  console.log("Hihi");
+const [command, key, value] = process.argv.slice(2);
+const fs = require("fs");
+
+function get() {
+  try {
+    const dbJSON = fs.readFileSync("db.json", "utf8");
+    const passwords = JSON.parse(dbJSON);
+    console.log(passwords["wifi"], passwords[key]);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-sayHi();
+function set() {
+  console.log("Called SET", key, value);
+  const dbJSON = fs.readFileSync("db.json", "utf8");
+  const passwords = JSON.parse(dbJSON);
+  passwords[key] = value;
+  console.log(passwords[key]);
+  try {
+    fs.writeFileSync("db.json", JSON.stringify(passwords, null, 4));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+if (command === "get") {
+  get();
+} else if (command === "set") {
+  set();
+} else {
+  console.log("Unkown command");
+}
