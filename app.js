f99fc83a39f7929792da387b2253aae8c10df301
Susanne1307 <60884180+Susanne1307@users.noreply.github.com>
2020-03-24 09:20:26 +0100

    Outsource commands and functions (#3)
    * Add passwords.js and outsource read and write functions
    
    * Outsource commands

--- app.js
+++ app.js
@@ -1,48 +1,12 @@
 const [command, key, value] = process.argv.slice(2);
-const fs = require("fs");
-
-function get() {
-  try {
-    const dbJSON = fs.readFileSync("db.json", "utf8");
-    const passwords = JSON.parse(dbJSON);
-    console.log(passwords["wifi"], passwords[key]);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function set() {
-  console.log("Called SET", key, value);
-  const dbJSON = fs.readFileSync("db.json", "utf8");
-  const passwords = JSON.parse(dbJSON);
-  passwords[key] = value;
-  console.log(passwords[key]);
-  try {
-    fs.writeFileSync("db.json", JSON.stringify(passwords, null, 2));
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-function unset() {
-  console.log("Called SET", key, value);
-  const dbJSON = fs.readFileSync("db.json", "utf8");
-  const passwords = JSON.parse(dbJSON);
-  delete passwords[key];
-  console.log(passwords[key]);
-  try {
-    fs.writeFileSync("db.json", JSON.stringify(passwords, null, 2));
-  } catch (error) {
-    console.error(error);
-  }
-}
+const { get, set, unset } = require("./lib/commands");
 
 if (command === "get") {
-  get();
+  get(key);
 } else if (command === "set") {
-  set();
+  set(key, value);
 } else if (command === "unset") {
-  unset();
+  unset(key);
 } else {
   console.log("Unkown command");
 }
